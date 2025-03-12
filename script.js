ca3e99054e23cab2db898caeab98c68b69c5855b
Ns05akhnez <157380434+Ns05akhnez@users.noreply.github.com>
2025-03-12 23:39:16 +0200

    js

--- script.js
+++ script.js
@@ -65,3 +65,49 @@ function updateSliderPosition(){
     const offset = -slideWidth * (currentIndex + 1);
     track.style.transform = `translateX(${offset}px)`;
 }
+
+function nextHeroSlide(){
+    if (heroIsMoving) return;
+    heroIsMoving = true;
+    heroCurrentIndex++;
+    updateHeroSliderPosition();
+
+    if (heroCurrentIndex === heroSlides.length){
+        setTimeout(()=>{
+            heroTrack.style.transition = 'none';
+            heroCurrentIndex = 0;
+            htroTrack.style.transform = `translateX(${
+                -heroSlideWidth * (heroCurrentIndex + 1)
+            }px)`;
+            heroIsMoving = false;
+        },500);
+    } else {
+        heroIsMoving = false;
+
+    }
+}
+
+setInterval(nextHeroSlide, 3000);
+
+heroTrack.style.transform = `translateX(${
+    -heroSlideWidth * ( heroCurrentIndex + 1)
+}px)`;
+
+const track = document.querySelector('.slider-track');
+const slides = Array.from(document.querySelectorAll('.slide'));
+const slideWidth = 616 + 40;
+let currentIndex = 0;
+let isMoving = false;
+
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
+
+track.appendChild(firstClone);
+track.insertBefore(lastClone, slides[0]);
+
+function updateSliderPosition(){
+    track.style.transition = 'transform 0.5s ease-in-out';
+    const offset = -slideWidth * (currentIndex + 1);
+    track.style.transform = `translateX(${offset}px)`;
+
+}
